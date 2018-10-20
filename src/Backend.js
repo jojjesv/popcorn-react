4b2adc459fb9d104f35ff95efd57d6e821c78b27
Johan Svensson <partlight.johans@gmail.co>
2018-10-20 16:14:59 +0200

    use of http methods

--- src/Backend.js
+++ src/Backend.js
@@ -1,28 +1,67 @@
 import 'whatwg-fetch';
 
+let dev = document.location.origin.indexOf('localhost') != -1;
+
 /**
  * Manages backend requests.
  * @author Johan Svensson
  */
 export default class Backend {
   static get baseUrl() {
-    return `${document.location.origin}/api`;
+    return `${dev ? 'http://192.168.98.96:3001' : document.location.origin.replace(":3000", ":3001")}/api`;
+  }
+
+  /**
+   * Performs a simple GET request.
+   * @param {string} path 
+   */
+  static async get(path) {
+    return this.request(path, null, 'get');
+  }
+
+  /**
+   * Performs a simple POST request.
+   * @param {string} path 
+   */
+  static async post(path, body) {
+    return this.request(path, body, 'post');
+  }
+
+  /**
+   * Performs a simple PUT request.
+   * @param {string} path 
+   */
+  static async put(path, body) {
+    return this.request(path, body, 'put');
+  }
+
+  /**
+   * Performs a simple DELETE request.
+   * @param {string} path 
+   */
+  static async delete(path) {
+    return this.request(path, null, 'get');
   }
 
   /**
    * Performs a regular request to the backend.
    * @param {string} path Relative path + query parameters, excluding leading question mark ("?")
    * @param {Object} post Optional POST parameters
+   * @param {"get"|"post"|"put"|"delete"} method HTTP method
    */
-  static async request(path, post) {
-    let useGet = !post || Object.keys(post).length == 0;
+  static async request(path, body, method) {
+    let useGet = !body || Object.keys(body).length == 0;
 
-    await (await window.fetch(`${Backend.baseUrl}${path}`, {
-      method: useGet ? "GET" : "POST",
+    if (useGet) {
+      method = 'get';
+    }
+
+    return await (await window.fetch(`${Backend.baseUrl}${path}`, {
+      method,
       headers: {
         "Content-Type": "application/json"
       },
-      body: useGet ? undefined : JSON.stringify(post)
+      body: useGet ? undefined : JSON.stringify(body)
     })).json();
   }
 }
