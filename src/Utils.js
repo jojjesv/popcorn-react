bcc4cdc6186df1aa3582e6a4f25976277cf69f48
Johan Svensson <partlight.johans@gmail.co>
2018-10-20 16:15:15 +0200

    fixed minutes -> str

--- src/Utils.js
+++ src/Utils.js
@@ -9,7 +9,16 @@ export default {
    * @param {number} minutes 
    */
   minutesToTimeString(minutes) {
-
+    let hrs = Math.floor(minutes / 60);
+    let mins = minutes % 60;
+    
+    if (hrs && mins) {
+      return `${hrs}${hrs == 1 ? 'hr' : 'hrs'} ${mins}min`;
+    } else if (hrs) {
+      return `${hrs}${hrs == 1 ? 'hr' : 'hrs'}`;
+    } else {
+      return `${mins}min`;
+    }
   },
 
   sleep(ms) {
