711d59be147c554c2dc4f9923cb3441b2066ffd7
Johan Svensson <partlight.johans@gmail.co>
2018-10-21 07:38:28 +0200

    added movie placeholders

--- src/common/grid_list/index.js
+++ src/common/grid_list/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './styles.scss';
+import './derived.scss';
 
 /** 
  * Common grid list. Imitates React Native's FlatList prop-wise.
